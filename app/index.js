4cc4f0a9487194de4bee24c48d060f54da64c868
Alex Bonder <lexbonder@gmail.com>
2018-11-20 18:53:14 -0500

    Update Dice-models to be single object exporting default as diceBag object
    Update rollDice funciton to be single reduce function returning total and array of rolledDice name + roll

--- app/index.js
+++ app/index.js
@@ -1,52 +1,68 @@
 import document from 'document';
-import { me } from "appbit";
-import * as diceBag from '../common/dice-models';
+import { me } from 'appbit';
+import diceBag from '../common/diceBag';
 
 const tr = document.getElementById('btn-tr');
 const br = document.getElementById('btn-br');
-const diceTumbler = document.getElementById("diceTumbler");
-const qtyTumbler = document.getElementById("qtyTumbler");
+const diceTumbler = document.getElementById('diceTumbler');
+const qtyTumbler = document.getElementById('qtyTumbler');
+const confirmScreen = document.getElementById('confirm');
+
+// ////////
+// let list = document.getElementById('my-list');
+// let items = list.getElementsByClassName('tile-list-item');
+
+// items.forEach((element, index) => {
+//   let touch = element.getElementById('touch-me');
+//   touch.onclick = evt => {
+//     console.log(`touched: ${index}`);
+//   };
+// });
+
+// ////////
 
 let chosenDie;
-const diceToRoll = []; // [{die: diceBag[d6], qty: 3}, {die: diceBag[d8], qty: 2}]
+const diceToRoll = []; // [{name: 'd6', qty: 3}, {name: 'd8', qty: 2}]
 
 diceTumbler.onclick = () => {
   const selectedIndex = diceTumbler.value;
-  const selectedItem = diceTumbler.getElementById("item" + selectedIndex);
-  const selectedDie = selectedItem.getElementById("content");
+  const selectedItem = diceTumbler.getElementById('item' + selectedIndex);
+  const selectedDie = selectedItem.getElementById('content');
   qtyTumbler.style.visibility = 'visible';
   diceTumbler.style.visibility = 'hidden';
   chosenDie = selectedDie.text;
-  console.log(chosenDie);
-}
+  console.log('Chosen Die: ' + chosenDie);
+};
 
 qtyTumbler.onclick = () => {
   const selectedIndex = qtyTumbler.value;
-  const selectedItem = qtyTumbler.getElementById("item" + selectedIndex);
-  const selectedQty = selectedItem.getElementById("content");
-  // qtyTumbler.style.visibility = "visible";
-  // diceTumbler.style.visibility = "hidden";
-  diceToRoll.push({die: diceBag[chosenDie], qty: parseInt(selectedQty.text, 10)})
-  console.log(diceToRoll[0].die, diceToRoll[0].qty)
+  const selectedItem = qtyTumbler.getElementById('item' + selectedIndex);
+  const selectedQty = selectedItem.getElementById('content');
+  qtyTumbler.style.visibility = 'hidden';
+  // confirmScreen.style.visibility = 'visible';
+  diceToRoll.push({ name: chosenDie, qty: parseInt(selectedQty.text, 10) });
+  console.log('result: ' + diceToRoll[0].name + ' ' + diceToRoll[0].qty);
 };
 
-// Either update this to reflect {die, qty} object or
-// modify full array of dice to fit this format
 const rollDice = dice => {
-  dice.map(die => {
-    die.roll = Math.floor(Math.random() * die.max) + 1
-    return die
-  })
-  const total = dice.reduce((total, die) => total + die.roll, 0);
-  return { total, roll: dice }
-}
+  return dice.reduce((result, die) => {
+    let { qty, name } = die;
+
+    for (let i = 0; i < qty; i++) {
+      let roll = Math.floor((Math.random() * diceBag[name].max) + 1)
+      result.total += roll;
+      result.rolledDice.push({ name, roll })
+    }
+    return result;
+  }, { total: 0, rolledDice: [] })
+};
 
 const coinFlip = () => {
-  return Math.random() > .5 ? 'Heads' : 'Tails';
-}
+  return Math.random() > 0.5 ? 'Heads' : 'Tails';
+};
 
 tr.onactivate = () => {
-  console.log(rollDice([diceBag.d20]).total)
+  console.log(rollDice([{ name: 'd20', qty: 1 }]).total);
 };
 
 br.onactivate = () => {
@@ -58,9 +74,12 @@ br.onactivate = () => {
 document.onkeypress = e => {
   e.preventDefault();
   if (e.key === 'back' && diceTumbler.style.visibility === 'visible') {
-    me.exit()
+    me.exit();
   } else if (e.key === 'back' && qtyTumbler.style.visibility === 'visible') {
-    diceTumbler.style.visibility = 'visible';
     qtyTumbler.style.visibility = 'hidden';
+    diceTumbler.style.visibility = 'visible';
+  } else if (e.key === 'back' && confirmScreen.style.visibility === 'visible') {
+    confirmScreen.style.visibility = 'hidden';
+    qtyTumbler.style.visibility = 'visible';
   }
-}
+};
---
+++ common/diceBag.js
@@ -0,0 +1,30 @@
+export default {
+  d4: {
+    name: 'd4',
+    max: 4,
+  },
+  d6: {
+    name: 'd6',
+    max: 6,
+  },
+  d8: {
+    name: 'd8',
+    max: 8,
+  },
+  d10: {
+    name: 'd10',
+    max: 10,
+  },
+  d100: {
+    name: 'd100',
+    max: 100,
+  },
+  d12: {
+    name: 'd12',
+    max: 12,
+  },
+  d20: {
+    name: 'd20',
+    max: 20,
+  },
+};
